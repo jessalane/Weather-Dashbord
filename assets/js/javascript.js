0ea94d4670452702092de1e3d8f837b652c1cdad
Jessica Lane <jessica.lane.a@gmail.com>
2022-07-25 17:11:15 -0600

    adding uv colors, fixing glitches with history buttons

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,7 @@
 var searchButton = $("#toggle");
 var asideSec = $("aside");
 var cityData = $("#cityData");
+var city = "";
 
 // SEARCH SECTION TOGGLE FUNCTION
 searchButton.click(function () {
@@ -24,32 +25,47 @@ searchButton.click(function () {
 
 // PULLING VARIABLES FROM LOCAL, AND PUTTING INTO A BUTTON
 var pulledSearches = window.localStorage.getItem("storedSearch");
-var splitSearches = pulledSearches.split(',');
-for (var i = 0; i < splitSearches.length; i++) {
-    $("hr").append("<button type=\"submit\" form=\"searchForm\" value=\"" + splitSearches[i] + "\">" + splitSearches[i] + "</button>");
+
+if(pulledSearches !== null) {
+    var splitSearches = pulledSearches.split(',');
+    for (var i = 0; i < splitSearches.length; i++) {
+        if(splitSearches[i].length !== 0) {
+            $("hr").append("<input type='button' class='historySearch' value='" + splitSearches[i] + "'>");
+        }
+    }
 }
+
+// STARTS populateWeather ON CLICKING .historySearch
+$(".historySearch").on("click", function() {
+    
+    city = $(this).prop('value');
+
+    populateWeather(city);
+    
+  });
 
 // CAPTURING THE SEARCH SUBMISSION INPUT
 var searchSub = $("#searchSub");
 
-// CREATING A FUNCTION TO POPULATE THE API URL ON SUBMISSION
+// STARTS populateWeather ON SUBMIT
 searchSub.click(function (event) {
-    // LOCAL VARIABLES
-    var city = $("#searchCity").val();
-    
-    if (city == null) {
-        $("button").click(function() {
-            city = $(this).text();
-        });
-    }
+    // PREVENTS THE DEFAULT CLEARING
+    event.preventDefault();
+
+    city = $("#searchCity").val();
+
+    populateWeather(city);
+
+})
+
+// CREATING A FUNCTION TO POPULATE THE API URL ON SUBMISSION
+function populateWeather() {
 
     // MY API KEY AND THE URL
     var APIKey = "9c4d817dd15f5df6514334a9cf6c9abe";
     var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&lang=en&q=" + city + "&appid=" + APIKey;
 
-    // PREVENTS THE DEFAULT CLEARING
-    event.preventDefault();
-
+    // SAVING SEARCH INFORMATION TO THE LOCAL STORAGE
     var storedSearch = localStorage.getItem("storedSearch");
     var newSearch = document.querySelector("#searchCity").value;
 
@@ -87,7 +103,7 @@ searchSub.click(function (event) {
             $("#wind").text("Wind: " + cityWind + " MPH");
             $("#humidity").text("Humidity: " + cityHumid + "%");
             $("#nameTitle").text(cityName + " (" + dateToday + ")");
-            $("#nameTitle").after(" <img src='http://openweathermap.org/img/wn/" + cityIcon + ".png'>");
+            $("#cityImg"). attr("src", "http://openweathermap.org/img/wn/" + cityIcon + ".png");
 
             // LON AND LAT VARIABLES
             var cityLon = data.coord.lon;
@@ -108,7 +124,24 @@ searchSub.click(function (event) {
                     var cityUV = data.current.uvi;
 
                     // ADDING UV INDEX TO THE CITY DATA SECTION
-                    $("#uv").text("UV Index: " + cityUV);
+                    $("#uvColor").text(cityUV);
+
+                    if(cityUV <= 2) {
+                        $("#uvColor").css({
+                            "display": "inline-block",
+                            "background-color" : "#008000"
+                        });
+                    } else if(cityUV >= 3 && cityUV <= 7) {
+                        $("#uvColor").css({
+                            "display": "inline-block",
+                            "background-color" : "#FBB829"
+                        });
+                    } else if (cityUV > 8) {
+                        $("#uvColor").css({
+                            "display": "inline-block",
+                            "background-color" : "#C62626"
+                        });
+                    }
 
                     // LOOPING THE WEEKLY FORECAST
                     for (i = 0; i < 5; i++) {
@@ -130,4 +163,4 @@ searchSub.click(function (event) {
 
                 })
         })
-})
+}
